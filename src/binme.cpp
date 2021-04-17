8674c040fdecd3380dfd02a0a05c9f7137975271
nico-castell <74460331+nico-castell@users.noreply.github.com>
2021-04-16 22:36:48 -0300

    :x: binme now prints simple output, not a table

--- src/binme.cpp
+++ src/binme.cpp
@@ -1,15 +1,6 @@
 #include <bitset>
 #include <iostream>
-#include <unistd.h>
 using std::string;
-
-// Only print escape sequence to format output if the output is a console.
-string cprty(const string& formatting)
-{
-	if (!isatty(fileno(stdout)))
-		return "";
-	return "\e[" + formatting + "m";
-}
 
 int main(int argc, char* argv[])
 {
@@ -26,23 +17,10 @@ int main(int argc, char* argv[])
 	string input = argv[1];
 	int il = input.length();
 
-	// Print headers for the table.
-	std::cout << cprty("32") << "Binary  \tDec\tHex\tOct\tChar" << cprty("00") << '\n';
-
 	// Print the table.
 	for (int i = 0; i < il; i++)
-	{
-		char character = input[i];
-		int charint = character;
-		std::bitset<8> binaryForm(character);
-		// Pretty print everything to the console.
-		std::cout << cprty("33") << binaryForm                   // Print the binary.
-					 << cprty("00") << '\t' << charint              // Print the decimal.
-					 << cprty("00") << '\t' << std::hex << charint  // Print the hexadecimal.
-					 << cprty("00") << '\t' << std::oct << charint  // Print the octal.
-					 << cprty("31") << '\t' << character            // Print the character.
-					 << cprty("00") << '\n';                        // End the line
-	}
+		std::cout << (std::bitset<8>)input[i] << ' ';
+	std::cout << '\n';
 
 	return 0;
 }
