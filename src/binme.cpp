f7b12acd6b77819e68e70abc6538eaa6677bfaf9
nico-castell <74460331+nico-castell@users.noreply.github.com>
2021-04-16 21:51:11 -0300

    :sparkles: Wrote binme program

--- src/binme.cpp
+++ src/binme.cpp
@@ -1,7 +1,48 @@
+#include <bitset>
 #include <iostream>
+#include <unistd.h>
+using std::string;
 
-int main()
+// Only print escape sequence to format output if the output is a console.
+string cprty(const string& formatting)
 {
-	std::cout << "Hello from binme!\n";
+	if (!isatty(fileno(stdout)))
+		return "";
+	return "\e[" + formatting + "m";
+}
+
+int main(int argc, char* argv[])
+{
+	// Validate arguments.
+	if (argc != 2)
+	{
+		string name = argv[0];
+		std::cerr << "\e[31mERROR: You must input one argument\n"
+					 << "\e[33mUsage:\e[00m " + name + " 'Phrase'\n";
+		return 1;
+	}
+
+	// The input from the user.
+	string input = argv[1];
+	int il = input.length();
+
+	// Print headers for the table.
+	std::cout << cprty("32") << "Binary  \tDec\tHex\tOct\tChar" << cprty("00") << '\n';
+
+	// Print the table.
+	for (int i = 0; i < il; i++)
+	{
+		char character = input[i];
+		int charint = character;
+		std::bitset<8> binaryForm(character);
+		// Pretty print everything to the console.
+		std::cout << cprty("33") << binaryForm                   // Print the binary.
+					 << cprty("00") << '\t' << charint              // Print the decimal.
+					 << cprty("00") << '\t' << std::hex << charint  // Print the hexadecimal.
+					 << cprty("00") << '\t' << std::oct << charint  // Print the octal.
+					 << cprty("31") << '\t' << character            // Print the character.
+					 << cprty("00") << '\n';                        // End the line
+	}
+
 	return 0;
 }
